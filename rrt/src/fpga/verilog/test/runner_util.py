950a3ef41137df3d21f55bcbe45bcc0251b64966
Yey007 <55263178+Yey007@users.noreply.github.com>
2025-12-25 01:28:30 -0600

    Start verilog

---
+++ rrt/src/fpga/verilog/test/runner_util.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from cocotb_tools.runner import get_runner
+
+
+def gen_test_runner(src: str, hdl_toplevel: str, module_name: str):
+    def test_runner():
+        sim = os.getenv("SIM", "icarus")
+
+        proj_path = Path(__file__).resolve().parent.parent
+
+        sources = [proj_path / "src" / src]
+
+        runner = get_runner(sim)
+        runner.build(
+            sources=sources,
+            hdl_toplevel=hdl_toplevel,
+            timescale=("1ns", "1ns"),
+            always=True,
+        )
+        runner.test(hdl_toplevel=hdl_toplevel, test_module=module_name)
+
+    return test_runner
---
+++ rrt/src/fpga/verilog/test/test_my_design2.py
@@ -0,0 +1,32 @@
+import cocotb
+from cocotb.triggers import FallingEdge, Timer
+from runner_util import gen_test_runner
+
+
+async def generate_clock(dut):
+    """Generate clock pulses."""
+
+    for _ in range(10):
+        dut.clk.value = 0
+        await Timer(1, unit="ns")
+        dut.clk.value = 1
+        await Timer(1, unit="ns")
+
+
+@cocotb.test()
+async def my_second_test(dut):
+    """Try accessing the design."""
+
+    cocotb.start_soon(generate_clock(dut))  # run the clock "in the background"
+
+    await Timer(5, unit="ns")  # wait a bit
+    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+
+    cocotb.log.info("my_signal_1 is %s", dut.my_signal_1.value)
+    assert dut.my_signal_2.value == 0
+
+
+test_my_design2 = gen_test_runner("my_design2.sv", "my_design2", "test_my_design2")
+
+if __name__ == "__main__":
+    test_my_design2()
